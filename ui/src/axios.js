2e5f810ffa4a900d0442a727b557c8aabae95310
QLe13 <94641669+QLe13@users.noreply.github.com>
2023-12-04 13:54:22 -0600

    Update axios.js

--- ui/src/axios.js
+++ ui/src/axios.js
@@ -1,7 +1,7 @@
 import a from 'axios';
 
 export const axios = a.create({
-    baseURL: 'http://localhost:9000/api/',
+    baseURL: '/api/',
     timeout: 5000,
     headers: {
         'Content-Type': 'application/json'
